00ff514c1a6c29b361ab647815b72fc11c68d951
hsnshehata <hsn.shehata@gmail.com>
2025-04-05 10:05:41 +0200

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { login } = require('../controllers/authController');
+const authenticateToken = require('../middleware/auth'); // إضافة الميدل وير
 
-router.post('/login', login);
+// إضافة المصادقة للتأكد من صحة التوكن قبل السماح بتسجيل الدخول
+router.post('/login', authenticateToken, login);  // استخدام المصادقة للتوكن
 
 module.exports = router;
