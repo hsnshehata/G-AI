64bc06aec7d173b04738b2d44e2afbf083688bc8
hsnshehata <hsn.shehata@gmail.com>
2025-04-08 10:38:26 +0200

    Update faq.js

--- server/routes/faq.js
+++ server/routes/faq.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { createFaq, getFaqs } = require('../controllers/faqController');
+const { createFaq, getFaqs, deleteFaq } = require('../controllers/faqController');
 
 router.post('/', createFaq);
 router.get('/', getFaqs);
+router.delete('/:id', deleteFaq); // إضافة endpoint للحذف
 
 module.exports = router;
