bf5b3482929a12656378a0624bc23a0841f50486
hsnshehata <hsn.shehata@gmail.com>
2025-04-08 03:40:59 +0200

    Update bots.js

--- server/routes/bots.js
+++ server/routes/bots.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const {
   createBot,
   listBots,
-  updateBot
+  updateBot,
+  getBotById
 } = require('../controllers/botsController');
 
 router.post('/create', createBot);
 router.get('/', listBots);
+router.get('/:id', getBotById); // ← ده المسار الجديد
 router.put('/:id', updateBot);
 
 module.exports = router;
