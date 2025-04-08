a6aa6eb9ecf6646602b10f0065578aae99ada55b
hsnshehata <hsn.shehata@gmail.com>
2025-04-08 10:40:13 +0200

    Update storeLink.js

--- server/routes/storeLink.js
+++ server/routes/storeLink.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { createStoreLink, getStoreLink } = require('../controllers/storeLinkController');
+const { createStoreLink, getStoreLink, deleteStoreLink } = require('../controllers/storeLinkController');
 
 router.post('/', createStoreLink);
 router.get('/', getStoreLink);
+router.delete('/:id', deleteStoreLink); // إضافة endpoint للحذف
 
 module.exports = router;
