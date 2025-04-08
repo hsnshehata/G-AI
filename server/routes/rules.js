a872beea2013dff33de6dae8a4178c5b65ff2f06
hsnshehata <hsn.shehata@gmail.com>
2025-04-08 05:45:50 +0200

    Update rules.js

--- server/routes/rules.js
+++ server/routes/rules.js
@@ -1,19 +1,13 @@
 const express = require('express');
 const router = express.Router();
-
 const {
   getRules,
   addRule,
   deleteRule
 } = require('../controllers/rulesController');
 
-// جلب القواعد (العامة + الخاصة)
 router.get('/', getRules);
-
-// إضافة قاعدة
 router.post('/', addRule);
-
-// حذف قاعدة
 router.delete('/:id', deleteRule);
 
 module.exports = router;
