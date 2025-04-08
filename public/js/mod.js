fa26c3b452e8739d7587b3b32cf5b49040e51c03
hsnshehata <hsn.shehata@gmail.com>
2025-04-08 12:23:35 +0200

    Update mod.js

--- public/js/mod.js
+++ public/js/mod.js
@@ -1,9 +1,7 @@
 const mod = {
   initializeTabs: function () {
     console.log('Tabs initialized successfully');
-    // هنا ممكن تضيف أي منطق إضافي لتهيئة التبويبات
   }
 };
 
-// نعرف mod في الـ global scope عشان dashboard.js يقدر يستخدمه
 window.mod = mod;
