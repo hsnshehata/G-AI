e1ad2b16a3108a6db842e49acd44852e18af3880
hsnshehata <hsn.shehata@gmail.com>
2025-04-09 08:30:25 +0200

    Update bots.js

--- server/routes/bots.js
+++ server/routes/bots.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { createBot, getBots } = require('../controllers/botsController');
+const {
+  createBot,
+  getBots,
+  updateBot,
+  deleteBot
+} = require('../controllers/botsController');
+
 const verifyToken = require('../middleware/verifyToken');
 
 router.post('/create', verifyToken, createBot);
 router.get('/', verifyToken, getBots);
+
+// ✅ المسارات الجديدة:
+router.put('/:id', verifyToken, updateBot);
+router.delete('/:id', verifyToken, deleteBot);
 
 module.exports = router;
