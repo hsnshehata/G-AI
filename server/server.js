9392b0b5fdf0eb8953f769fa4abc491c6275b5de
hsnshehata <hsn.shehata@gmail.com>
2025-04-11 10:00:58 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,20 +7,17 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
-// Connect to MongoDB
 connectDB();
 
-// Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/bots', require('./routes/bots'));
 app.use('/api/users', require('./routes/users'));
+app.use('/api/rules', require('./routes/rules'));
 
-// Serve Dashboard
 app.get('/dashboard', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/dashboard.html'));
 });
