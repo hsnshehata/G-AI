ae003e38da83707c8da17348e29a3303c5f1bbb9
hsnshehata <hsn.shehata@gmail.com>
2025-04-11 09:56:46 +0200

    Update Bot.js

--- server/models/Bot.js
+++ server/models/Bot.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const botSchema = new mongoose.Schema({
   name: { type: String, required: true },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  facebookApiKey: { type: String, default: null },
+  facebookPageId: { type: String, default: null },
   createdAt: { type: Date, default: Date.now },
 });
 
