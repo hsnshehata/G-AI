96927562b21626782a67fe52537f872445b5fbe1
hsnshehata <hsn.shehata@gmail.com>
2025-04-11 08:19:24 +0200

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { loginUser } = require('../controllers/authController');
+const authController = require('../controllers/authController');
 
-router.post('/', loginUser);
+router.post('/login', authController.login);
 
 module.exports = router;
