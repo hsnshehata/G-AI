c7cce431f8347235b11d67fb523a580d12f2f31e
hsnshehata <hsn.shehata@gmail.com>
2025-04-14 05:36:32 +0200

    Delete server directory

--- server/routes/facebook.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { handleMessage } = require('../controllers/facebookController');
-
-// Webhook لاستقبال الرسائل من فيسبوك
-router.post('/', handleMessage);
-
-module.exports = router;
